302506e8652672c4f28f74f55456c34264594cc3
DARWICH ALI <adarwich@etu.unistra.fr>
2022-10-22 17:00:24 +0200

    m

--- ordonnanceur.py
+++ ordonnanceur.py
@@ -1,29 +1,48 @@
 class ordonnanceur(object):
     """docstring for ordonnanceur."""
 
-    lenght_array = 10080 # 7 jour * 24h * 60 mins
-    array = [lenght_array] # 0 creneau libre  sinon 1
+    length_array = 10080 # 7 jour * 24h * 60 mins
+    array = [length_array] # 0 creneau libre  sinon 1  -1 si date block
     vitesse_imprimmer = 1 # 1 page/sec
-    array = [0 for i in range(lenght_array)]
+    array = [0 for i in range(length_array)]
 
 
     def __init__(self):
         super(ordonnanceur, self).__init__()
-        array = [0 for i in range(lenght_array)]
+        self.array = [0 for i in range(self.length_array)]
 
-    def add_creneau(nb_pages):
-        for i in range(lenght_array):
-            if(array[i] == 0):
-                if(nb_pages+i < lenght_array):
+    def add_creneau(self, nb_pages):
+        if(nb_pages <= 0):
+            print("error on number of pages")
+            return
+        for i in range(self.length_array):
+            if(self.array[i] == 0):
+                if(nb_pages+i <= self.length_array):
                     print('reserving time')
-                    array = [1 for h in range(i,nb_pages+i)]
+                    # self.array[self.array >= i & self.array < (nb_pages*vitesse_imprimmer)+i] = [1 for h in range(i,nb_pages+i)]
+                    # self.array.loc[(self.array == -1) & (self.array == -1)]
+                    for h in range(i,(nb_pages*self.vitesse_imprimmer)+i) :
+                        self.array[h] = 1
+                    break
                 else :
                     print('il y a plus de creneau ')
+                    break
+            else :
+                print('il y a plus de creneau ')
+                break
+
+    # block a specific date
+    def bloque(self,jour):
+        len_day = self.length_array /7
+        self.array = [-1 for i in range(len_day*jour,len_day*jour+1)]
 
 
 
 def main():
-    pass
+
+    # x = ordonnanceur()
+    # x.add_creneau(10080)
+    # pass
 
 
 
