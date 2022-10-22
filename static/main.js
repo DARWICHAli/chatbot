98eef39862449cbd6be3df45048f8fd1490e6286
qadadri <q.haytam@gmail.com>
2022-10-22 16:21:20 +0200

    Chatbot UI connected to flask app

--- static/main.js
+++ static/main.js
@@ -28,7 +28,7 @@ $(function() {
     var str="";
     str += "<div id='cm-msg-"+INDEX+"' class=\"chat-msg "+type+"\">";
     str += "          <span class=\"msg-avatar\">";
-    str += "            <img src=\"https:\/\/image.crisp.im\/avatar\/operator\/196af8cc-f6ad-4ef7-afd1-c45d5231387c\/240\/?1483361727745\">";
+    str += "            <img src=\"./static/images/ali.png\">";
     str += "          <\/span>";
     str += "          <div class=\"cm-msg-text\">";
     str += msg;
@@ -62,7 +62,7 @@ $(function() {
     var str="";
     str += "<div id='cm-msg-"+INDEX+"' class=\"chat-msg user\">";
     str += "          <span class=\"msg-avatar\">";
-    str += "            <img src=\"https:\/\/image.crisp.im\/avatar\/operator\/196af8cc-f6ad-4ef7-afd1-c45d5231387c\/240\/?1483361727745\">";
+    str += "            <img src=\"./static/images/bot.png\">";
     str += "          <\/span>";
     str += "          <div class=\"cm-msg-text\">";
     str += msg;
